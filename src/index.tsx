f8d9cc526e7e14bfe854a9df3b2706a5023a1b58
Michal <michal.irzyylowski@gmail.com>
2020-12-30 10:52:35 +0100

    add HMR

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,8 @@ import { hydrate } from "react-dom";
 import { App } from "./app";
 
 hydrate(<App />, document.getElementById("app"));
+
+if (module.hot) {
+    module.hot.accept();
+    console.log("HMR enabled");
+}
