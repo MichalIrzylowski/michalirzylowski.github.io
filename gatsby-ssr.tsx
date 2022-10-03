3e40c44c037424fa39ff385e8fc7ca0793a27743
Michal <mirzylowski@griddynamics.com>
2022-10-03 17:48:49 +0200

    fix font flickering

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -9,9 +9,17 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = ({
             rel="preload"
             href="/fonts/Lato-Regular.ttf"
             as="font"
-            type="font/truetype"
+            type="font/ttf"
             crossOrigin="anonymous"
             key="interFont"
+        />,
+        <link
+            rel="preload"
+            href="/fonts/Lato-Bold.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+            key="boldFont"
         />,
     ]);
 };
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,10 +2,15 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
     @font-face {
-        font-family: "Outfit";
-        font-weight: 100 900;
+        font-family: "Outfit-regular";
         font-display: swap;
         src: url(/fonts/Lato-Regular.ttf) format("truetype");
+    }
+
+    @font-face {
+        font-family: "Outfit-bold";
+        font-display: swap;
+        src: url(/fonts/Lato-Bold.ttf) format("truetype");
     }
 
     * {
@@ -14,6 +19,10 @@ const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
         font-family: "Outfit-regular", sans-serif;
 
+    }
+
+    strong {
+        font-family: 'Outfit-bold', sans-serif;
     }
 
     body {
@@ -26,7 +35,6 @@ const GlobalStyles = createGlobalStyle`
 
         @media print {
             padding: 0;
-            print-color-adjust: exact;
         }
     }
 `;
